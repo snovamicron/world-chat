3f41c045ba750ff166929d9c71c7216c98d9d2f9
snovamicron <mdip5675@gmail.com>
2022-04-08 21:23:42 +0530

    v 0.0.0

--- client/src/components/Main/Home/Appbar.tsx
+++ client/src/components/Main/Home/Appbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './Appbar.css'
 
 // MUI components
@@ -15,24 +15,27 @@ import { DataContext } from '../../../context/DataContext'
 
 
 const Appbar = () => {
-    const {setMessageReciver} = useContext(DataContext)
-    const goHome = ():void=>{
+    const { setMessageReciver } = useContext(DataContext)
+    const goHome = (): void => {
         setMessageReciver(null)
     }
     const user = JSON.parse(localStorage.getItem('user'))
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
-                <AppBar  position="static" sx={{backgroundColor:'#fff'}}>
-                    <Toolbar sx={{justifyContent:'space-between'}}>
-                        <Box sx={{display:'flex', cursor:'pointer'}} onClick={goHome}>
-                        <img className='logo' src='bird.png' alt='logo'/>
-                        <Typography sx={{color:'#000', fontSize:'1.8rem', marginLeft:'8px'}}>World Chat</Typography>
+                <AppBar position="static" sx={{ backgroundColor: '#fff' }}>
+                    <Toolbar sx={{ justifyContent: 'space-between' }}>
+                        <Box sx={{ display: 'flex', cursor: 'pointer' }} onClick={goHome}>
+                            <img className='logo' src='bird.png' alt='logo' />
+                            <Typography sx={{ color: '#000', fontSize: '1.8rem', marginLeft: '8px' }}>World Chat</Typography>
                         </Box>
-                        <Box sx={{display:'flex', cursor:'pointer', alignItems:'center', marginRight:2}} onClick={goHome}>
-                            <AccountCircleOutlinedIcon sx={{color:'#9eaba0', fontSize:'2rem', marginRight:1}}/>
-                        <Typography sx={{color:'#000', fontSize:'1.2rem'}}>{user.name}</Typography>
-                        </Box>
+                        {
+                            user &&
+                            <Box sx={{ display: 'flex', cursor: 'pointer', alignItems: 'center', marginRight: 2 }} onClick={goHome}>
+                                <AccountCircleOutlinedIcon sx={{ color: '#9eaba0', fontSize: '2rem', marginRight: 1 }} />
+                                <Typography sx={{ color: '#000', fontSize: '1.2rem' }}>{user.name}</Typography>
+                            </Box>
+                        }
                     </Toolbar>
                 </AppBar>
             </Box>
